181f920454fdf2f06dd6fe9d99f8223c1fb1f4ec
Paula Scatarelli <paulascatarelli@gmail.com>
2021-12-11 19:24:25 -0300

    ejercicio sobre pizzas e ingredientes

---
+++ pizza.py
@@ -0,0 +1,20 @@
+ingredientescomunes = ['mozzarella', 'tomate']
+ingredientesveggies = ['pimiento', 'tofu']
+ingredientesnoveggies = ['peperoni', 'jamón', 'salmón']
+
+pedido = input('\n Buenas noches, por favor ingrese "V" para pizza veggie y "C" para pizza común: \n')
+
+
+print('Su pizza contiene los siguientes ingredientes: \n')
+
+if pedido == 'v':
+    for i in ingredientescomunes:
+        print(i)
+    for i in ingredientesveggies:
+        print(i)
+   
+if pedido == 'c':
+    for i in ingredientescomunes:
+        print(i)
+    for i in ingredientesnoveggies:
+        print(i)
