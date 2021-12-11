a16180c68b10bac2268ab18c6efb255746f9687f
Paula Scatarelli <paulascatarelli@gmail.com>
2021-12-11 20:36:19 -0300

    primer ejercicio con for

---
+++ buclepalabra.py
@@ -0,0 +1,14 @@
+
+"""
+Ejercicios de Bucles
+Ejercicio 1
+Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+
+"""
+
+palabra = input('\n Hola, introducí una palabra de tu agrado: ')
+
+print('\n')
+
+for i in range(10):
+    print(palabra)
