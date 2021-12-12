d5d416e6502135e02912f094f9f4b4c620056249
Paula Scatarelli <paulascatarelli@gmail.com>
2021-12-12 18:22:47 -0300

    tabla del diez

---
+++ tabladeldiez.py
@@ -0,0 +1,8 @@
+"""Ejercicio 7
+Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+
+
+"""
+
+for i in range(11):
+        print(str(i) + ' x ' + ' 10 = ' + str(i * 10 ))
